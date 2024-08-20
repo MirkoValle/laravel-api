d32306ddccc52e174d0f46b34b93084710c3c1a8
MirkoValle <mirko.valle20@gmail.com>
2024-08-20 19:50:18 +0200

    Added Api controller with index and show function

---
+++ app/Http/Controllers/Api/ProjectController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Project;
+use Illuminate\Http\Request;
+
+class ProjectController extends Controller
+{
+    public function index(){
+
+        $projects = Project::with("type", "technologies")->paginate(10);
+
+        return response()->json(
+        [
+            'success' => true,
+            'results' => $projects
+        ]);
+
+    }
+
+    public function show(Project $project){
+
+        $project->loadMissing("type", "technologies");
+
+        return response()->json([
+            'success' => true,
+            'results' => $project
+        ]);
+    }
+}
